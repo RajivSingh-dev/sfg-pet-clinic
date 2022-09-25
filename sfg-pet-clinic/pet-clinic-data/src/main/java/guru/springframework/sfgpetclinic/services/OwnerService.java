ed8db7060a666269650fa870b8fa262c73af052a
Rajiv Singh <rajiv.singh@aas.services>
2022-09-25 16:26:16 +0530

    adding bootstrap for data.

--- sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/OwnerService.java
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/OwnerService.java
@@ -1,7 +1,8 @@
 package guru.springframework.sfgpetclinic.services;
 
 
-import java.security.acl.Owner;
+import guru.springframework.sfgpetclinic.model.Owner;
+
 import java.util.Set;
 
 public interface OwnerService extends CrudService<Owner,Long>{
