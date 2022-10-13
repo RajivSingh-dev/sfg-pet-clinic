848dd6c923479d4ec11fadda80a5e744750f15d8
rajiv <rs481441@gmail.com>
2022-10-13 09:08:34 +0530

    test

--- sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -3,7 +3,7 @@ package guru.springframework.sfgpetclinic.model;
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
-
+//
 @Entity
 @Table(name = "vets")
 public class Vet extends Person{
