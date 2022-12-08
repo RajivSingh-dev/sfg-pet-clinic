4a1e54b0fad43ce515a056b2c80574cc4988dcd9
rajiv <rs481441@gmail.com>
2022-12-08 12:15:01 +0530

    update

--- sfg-pet-clinic/pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/IndexController.java
+++ sfg-pet-clinic/pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/IndexController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
 
-    @GetMapping("/home")
+    @GetMapping("")
 public String Index()
 {
     return "index";
