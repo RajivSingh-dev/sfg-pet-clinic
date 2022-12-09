71999ea967c1a0ffb88708dbbbb51bd60ee4f5d7
rajiv <rs481441@gmail.com>
2022-12-09 12:45:11 +0530

    update

--- sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/PetReopistory.java
+++
@@ -1,9 +0,0 @@
-package guru.springframework.sfgpetclinic.repositories;
-
-import guru.springframework.sfgpetclinic.model.Pet;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PetReopistory extends CrudRepository<Pet,Long> {
-
-
-}
